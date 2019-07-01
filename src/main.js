6f76d64ba40abda7c42461320bcae8b584f5f8c7
Ping-k <ping_isvip@163.com>
2019-07-01 17:44:23 +0800

    新增Element-UI使用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import Element from 'element-ui'
+// 切记！必须引用样式资源，否则无法正常显示
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(Element) // 全局使用ElementUI
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
