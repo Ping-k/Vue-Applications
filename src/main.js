19cb010eddc4da86f028766acbc3c641ea08ce3e
Ping-k <ping_isvip@163.com>
2019-07-02 10:02:29 +0800

    更新elementUI注释

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// cnpm install element-ui -S
 import Element from 'element-ui'
 // 切记！必须引用样式资源，否则无法正常显示
 import 'element-ui/lib/theme-chalk/index.css'
